98b695f9abc30d7756b008587f695ddf1909d320
unknown <alexandru.sendrea@gmail.com>
2016-03-08 11:05:43 +0200

    Am creat MyController cu ex. din curs; la fel orasSablon si am adaugat o ruta noua in yml

---
+++ src/AppBundle/Controller/MyController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MyController extends Controller{
+    /**
+     * @Route ("/oras", name="afiseaza-oras")
+     * 
+     */
+    public function orasAction() {
+        $nume_oras="Bucuresti";
+        
+        $res = new Response(
+                "<html><body><p>Numele orasului este $nume_oras </h1></body></html>"
+                );
+                
+        return $res;
+    }
+    
+    /**
+     * @Route ("/orasSablon", name="oras-sablon")
+     * 
+     */
+    
+    
+    public function orasSablonAction() {
+        $nume_oras="Cluj";
+        
+        $res = $this->render('my/orasSablon.html.twig', 
+                array('oras' => $nume_oras)
+                );
+        return $res;
+    }
+    
+    public function afiseazaParamAction($p1,$p2) {
+        $res = new Response("<html><body><p>Cele doua orase sunt: $p1, $p2</p></body></html>");
+        return $res;
+            
+    }
+}
